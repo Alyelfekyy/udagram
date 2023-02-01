e7cdc1a3b3472aceaa03d441440981904bbf659f
alynamlaa <alynamlaa@gmail.com>
2023-02-01 04:48:53 +0200

    Update karma.conf.js

--- udagram/udagram-frontend/src/karma.conf.js
+++ udagram/udagram-frontend/src/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        browsers: ['C:Program Files (x86)GoogleChromeApplicationchrome.exe'],
+        browsers: ['Chrome'],
         singleRun: false,
     })
 }
